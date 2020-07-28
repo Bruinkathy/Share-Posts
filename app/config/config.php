f3a6bbc60733e39292e94697b2881db81b1d94d2
Kathleen Bartholomew <32341272+Bruinkathy@users.noreply.github.com>
2020-07-28 12:49:11 -0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'Secret123');
+define('DB_NAME', 'share_posts');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost:8080/share-posts');
+
+// Site Name
+define('SITENAME', 'SharePosts');
+
+// App Version
+define('APPVERSION', '1.0.0');
